fcbeefba4562a3dce7a06c54010e33ae7dd7b009
black <black@example.com>
2026-07-11 19:14:43 +0500

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,14 @@
+﻿using System; 
+
+class Program 
+{ 
+    static void Main(string[] args) 
+    { 
+        SayHello(); 
+    } 
+
+    static void SayHello() 
+    { 
+        Console.WriteLine("hello"); 
+    } 
+}
